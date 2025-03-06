f25e954e9ead17fad9dddea97522757cdaa69cfa
Fahmi <fahmi@rnd.ts.co.id>
2025-03-06 23:06:08 +0700

    feat: layouting orderbook

---
+++ app/pages/orderbook/components/item-clob.tsx
@@ -0,0 +1,36 @@
+import { cn } from "~/lib/utils";
+
+export type ItemClobProps = {
+	apy: string;
+	collateral: string;
+	type: string;
+};
+
+export default function ItemClob({ apy, collateral, type }: ItemClobProps) {
+	const handleButtonClick = () => {
+		console.log(apy, collateral);
+	};
+	return (
+		<button
+			type="button"
+			onClick={handleButtonClick}
+			className={cn(
+				"w-full my-[2px] text-sm px-2 py-[2px] flex items-center justify-between",
+				type === "borrow" ? "bg-green-500/10" : "bg-red-500/10",
+			)}
+		>
+			<span
+				className={cn(
+					type === "borrow"
+						? "text-green-500 hover:font-bold"
+						: "text-red-500 hover:font-bold",
+				)}
+			>
+				{`${apy}%`}
+			</span>
+			<span className="text-white bg-transparent border-none outline-none hover:font-bold">
+				{collateral}
+			</span>
+		</button>
+	);
+}
